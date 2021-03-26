2f46e43e5809b8826027819088e6308ba4f7a8c9
Mikaela Gabriels <mikaelagabriels@gmail.com>
2021-03-26 10:41:37 +0200

    Final commit

---
+++ Assignment8/Assignment8/Assignment8/Models/CustomerInformation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment8
+{
+    class CustomerInformation
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+
+        public string Tshirt_Size { get; set; }
+
+        public string Tshirt_Color { get; set; }
+
+        public string Shipping_Address { get; set; }
+
+        
+    }
+}
---
+++ Assignment8/Assignment8/Assignment8/Models/Shop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment8
+{
+    public class Shop
+    {
+        public string StoreName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string StoreLocation{ get; set; }
+        
+
+        private List<CustomerInformation> ShoppingCustomers { get; set; }
+
+
+
+        public Shop(string storeName, string phoneNumber, string storeLocation)
+        {
+            StoreName = storeName;
+            PhoneNumber = phoneNumber;
+            StoreLocation = storeLocation;
+        }
+    }
+}
